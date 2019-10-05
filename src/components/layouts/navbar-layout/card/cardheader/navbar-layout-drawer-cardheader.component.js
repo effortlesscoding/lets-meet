f077fcbac7cf3010196798e82df1e388b1cf3c3f
Anh D <frankensteinruo@gmail.com>
2019-10-05 19:12:43 +1000

    Added my avatar.

--- src/components/layouts/navbar-layout/card/cardheader/navbar-layout-drawer-cardheader.component.js
+++ src/components/layouts/navbar-layout/card/cardheader/navbar-layout-drawer-cardheader.component.js
@@ -1,19 +1,15 @@
-import React from 'react'
-import classes from './navbar-layout-drawer-cardheader.module.scss'
+import React from 'react';
+import classes from './navbar-layout-drawer-cardheader.module.scss';
 
+export const CardHeader = (props) => {
+  console.log(props.name);
+  var style = {
+    backgroundImage: `url(${require('@/assets/images/avatar.jpeg')})`
+  };
 
-export const CardHeader = props => {
-
-    console.log(props.name)
-    var style = {
-        backgroundImage: 'url(https://source.unsplash.com/user/erondu/600x400)',
-    };
-
-    return (
-        <header 
-        style={style}
-        className={classes.cardHeader}>
-            <h4 className={classes.cardHeaderTitle}>Mike</h4>
-        </header>
-    )
-}
+  return (
+    <header style={style} className={classes.cardHeader}>
+      <h4 className={classes.cardHeaderTitle}>Mike</h4>
+    </header>
+  );
+};
